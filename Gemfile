a9edd4dfefa225f327cd63b16bd472831c1e87df
acropolice <60513832+acropolicejp@users.noreply.github.com>
2022-10-30 17:04:12 +0900

    フロントエンド環境構築 (#2)
    * webpack関連の設定記述
    
    * simpacker導入
    
    * gitignore
    
    * simpackerの設定とmanifest.jsonの自動生成
    
    * rails側の設定
    
    * デバッガー/rubocop追加
    
    Co-authored-by: rukachess <pc.siomi@gmail.com>

--- Gemfile
+++ Gemfile
@@ -21,7 +21,12 @@ end
 
 group :development do
   gem 'listen', '~> 3.3'
+  gem 'debase'
+  gem 'ruby-debug-ide'
+  gem 'rubocop', '~> 1.22'
+  gem 'rubocop-rails', '~> 2.12'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'simpacker'
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const WebpackAssetsManifest = require("webpack-assets-manifest");
+
+const { NODE_ENV } = process.env;
+const isProd = NODE_ENV === "production";
+
+module.exports = {
+  mode: isProd ? "production" : "development",
+  entry: {
+    'application': path.resolve(__dirname, "app/javascript/packs/application.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "public/packs"),
+    publicPath: "/packs/",
+    filename: isProd ? "[name]-[hash].js" : "[name].js"
+  },
+  plugins: [
+    new WebpackAssetsManifest({
+      publicPath: true,
+      output: "manifest.json",
+    })
+  ]
+}
