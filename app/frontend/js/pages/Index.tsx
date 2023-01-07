8ec00d9b7963eafcb8f66c26297abac6a769c84d
KatsuKiNG <pausejapan@gmail.com>
2023-01-07 15:58:10 +0900

    showページを実装 (#27)
    * shopページ実装
    
    * robocop修正
    
    * showページへのリンクを修正
    
    * 再実装
    
    * ファイル名変更
    
    * コンフリクト修正

--- app/frontend/js/pages/Index.tsx
+++ app/frontend/js/pages/Index.tsx
@@ -1,19 +1,24 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Header from '../components/shared/header';
 import List from '../components/girls/List';
 import Detail from '../components/girls/Detail';
 import '../../stylesheets/application.scss';
 
 const Index: React.FC = () => {
   return (
-    <Switch>
-      <Route exact path="/girls">
-        <List />
-      </Route>
-      <Route path="/girls/:id(\d+)">
-        <Detail />
-      </Route>
-    </Switch>
+    <>
+      <Header />
+
+      <Switch>
+        <Route exact path="/girls">
+          <List />
+        </Route>
+        <Route path="/girls/:id(\d+)">
+          <Detail />
+        </Route>
+      </Switch>
+    </>
   );
 };
 
