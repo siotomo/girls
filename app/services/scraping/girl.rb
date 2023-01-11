91ba4cd28dce840febfc0ffbd8fadcbdcb5fe5af
acropolicejp <acropojp515@icloud.com>
2023-01-11 21:22:50 +0900

    rubocop

--- app/services/scraping/girl.rb
+++ app/services/scraping/girl.rb
@@ -1,68 +1,69 @@
 # frozen_string_literal: true
 
-class Scraping::Girl
-  def initialize(city_heven_id, driver)
-    @driver = driver
-    @city_heven_id = city_heven_id
-  end
+module Scraping
+  class Girl
+    def initialize(city_heven_id, driver)
+      @driver = driver
+      @city_heven_id = city_heven_id
+    end
 
-  def scrape_and_save_data
-    p "start_scraping: #{@city_heven_id}"
-    girl_show_url = "#{Scraping::Scraper::CITY_HEAVEN_DOMAIN}tokyo/A1316/A131603/ultra-galaxy/girlid-#{@city_heven_id}/"
-    @driver.get girl_show_url
-    girl = Girl.find_or_create_by({
-      name: name,
-      age: age,
-      tall: tall,
-      bust: bust,
-      west: west,
-      hip: hip,
-      score: 20,
-      store_id: 1,
-      city_heven_id: @city_heven_id
-    })
+    def scrape_and_save_data
+      Rails.logger.debug "start_scraping: #{@city_heven_id}"
+      girl_show_url = "#{Scraping::Scraper::CITY_HEAVEN_DOMAIN}tokyo/A1316/A131603/ultra-galaxy/girlid-#{@city_heven_id}/"
+      @driver.get girl_show_url
+      girl = Girl.find_or_create_by({
+                                      name: name,
+                                      age: age,
+                                      tall: tall,
+                                      bust: bust,
+                                      west: west,
+                                      hip: hip,
+                                      score: 20,
+                                      store_id: 1,
+                                      city_heven_id: @city_heven_id
+                                    })
 
-    shift = Scraping::Shift.new(@city_heven_id, @driver, girl.id)
-    shift_attributes = shift.build_attributes
-    Shift.insert_all(shift_attributes) if shift_attributes.present?
-    p "finished_scraping: #{@city_heven_id}"
-  end
+      shift = Scraping::Shift.new(@city_heven_id, @driver, girl.id)
+      shift_attributes = shift.build_attributes
+      Shift.insert_all(shift_attributes) if shift_attributes.present?
+      Rails.logger.debug "finished_scraping: #{@city_heven_id}"
+    end
 
-  private
+    private
 
-  def name
-    @driver.find_element(:id, "p_data").find_elements(tag_name: "td")[0].text
-  end
+    def name
+      @driver.find_element(:id, 'p_data').find_elements(tag_name: 'td')[0].text
+    end
 
-  def age
-    @driver.find_element(:id, "p_data").find_elements(tag_name: "td")[1].text.gsub('歳', '')
-  end
+    def age
+      @driver.find_element(:id, 'p_data').find_elements(tag_name: 'td')[1].text.gsub('歳', '')
+    end
 
-  def tall
-    girls_proportion.match(/T(\d{3})/)[1]
-  end
+    def tall
+      girls_proportion.match(/T(\d{3})/)[1]
+    end
 
-  def bust
-    girls_proportion.match(/\([A-Z]\)/)[0].gsub("(", "").gsub(")", "")
-  end
+    def bust
+      girls_proportion.match(/\([A-Z]\)/)[0].gsub('(', '').gsub(')', '')
+    end
 
-  def west
-    girls_proportion.split("・")[2]
-  end
+    def west
+      girls_proportion.split('・')[2]
+    end
 
-  def hip
-    girls_proportion.split("・")[3]
-  end
+    def hip
+      girls_proportion.split('・')[3]
+    end
 
-  def remove_space(text)
-    text.gsub(/\s/, '')
-  end
+    def remove_space(text)
+      text.gsub(/\s/, '')
+    end
 
-  def girls_proportion
-    remove_space(@driver.find_element(:id, "p_data").find_elements(tag_name: "td")[2].text)
+    def girls_proportion
+      remove_space(@driver.find_element(:id, 'p_data').find_elements(tag_name: 'td')[2].text)
+    end
   end
 end
-
 
 # # 出勤日
 # dls = prefecture_index_html.css('#girlprofile_right ul#girl_sukkin > li > dl')
--- app/services/scraping/scraper.rb
+++ app/services/scraping/scraper.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
-class Scraping::Scraper
-  CITY_HEAVEN_DOMAIN = 'https://www.cityheaven.net/'
-  ADULT_STORE_PATH = %w(
-    tokyo/A1316/A131603/ultra-galaxy/
-  )
+module Scraping
+  class Scraper
+    CITY_HEAVEN_DOMAIN = 'https://www.cityheaven.net/'
+    ADULT_STORE_PATH = %w[
+      tokyo/A1316/A131603/ultra-galaxy/
+    ].freeze
 
-  def initialize; end
+    def initialize; end
 
-  def exec
-    ADULT_STORE_PATH.each do |store_path|
-      store_url = "#{CITY_HEAVEN_DOMAIN}#{store_path}"
+    def exec
+      ADULT_STORE_PATH.each do |store_path|
+        store_url = "#{CITY_HEAVEN_DOMAIN}#{store_path}"
 
-      store = Scraping::AdultStore.new(store_url)
-      store.scrape_girl_list
+        store = Scraping::AdultStore.new(store_url)
+        store.scrape_girl_list
+      end
     end
   end
 end
